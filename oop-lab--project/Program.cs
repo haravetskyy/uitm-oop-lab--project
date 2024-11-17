aed35ed77030b839bcec6ec73ac5acd36d12ce9e
yuriyharavetskyy <yuriyharavetskyy@gmail.com>
2024-11-17 01:03:06 +0100

    Completing the tasks for the second laboratory work, which included the basic concepts of OOP

---
+++ oop-lab--project/Program.cs
@@ -0,0 +1,19 @@
+﻿using oop_lab__project.Constructions;
+
+namespace oop_lab__project.Program 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Construction myBuilding = new Construction(15.0f, 30.0f, 3, 200, "");
+            
+            Console.WriteLine($"Building Info:");
+            Console.WriteLine($"Height: {myBuilding.Height}");
+            Console.WriteLine($"Width: {myBuilding.Width}");
+            Console.WriteLine($"Entrances: {myBuilding.Entrances}");
+            Console.WriteLine($"Human Capacity: {myBuilding.HumanCapacity}");
+            Console.WriteLine($"Material: {myBuilding.BuildMaterial}");
+        }
+    }
+}
